b5042589117d1365d549c6adede6b0c7f8e69fb3
LucHighwalker <luc.highwalker@gmail.com>
2018-09-17 13:45:38 -0700

    - Finished step 4.

--- superheroes.py
+++ superheroes.py
@@ -1,21 +1,59 @@
 import random
 
+
 class Hero:
     name = ''
     abilities = list()
+    armors = list()
+    start_health = 0
+    health = 0
+    deaths = 0
+    kills = 0
 
-    def __init__(self, name):
+    def __init__(self, name, health=100):
         self.name = name
         self.abilities = list()
+        self.armors = list()
+        self.start_health = health
+        self.health = health
+        self.deaths = 0
+        self.kills = 0
 
     def add_ability(self, ability):
         self.abilities.append(ability)
 
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
     def attack(self):
         attack_power = 0
+
+        if self.health <= 0:
+            return 0
+
         for ability in self.abilities:
             attack_power += ability.attack()
         return attack_power
+
+    def defend(self):
+        defense_power = 0
+
+        if self.health <= 0:
+            return 0
+
+        for armor in self.armors:
+            defense_power += armor.defend()
+        return defense_power
+
+    def take_damage(self, damage_amt):
+        self.health -= damage_amt
+        if self.health <= 0:
+            return 1
+        return 0
+
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+
 
 class Ability:
     name = ''
@@ -32,10 +70,23 @@ class Ability:
     def update_attack(self, attack_strength):
         self.attack_strength = attack_strength
 
+
 class Weapon(Ability):
     def attack(self):
-        rand_val = random.randint(0, self.attack_strength)
-        return rand_val
+        return random.randint(0, self.attack_strength)
+
+
+class Armor:
+    name = ''
+    defense = 0
+
+    def __init__(self, name, defense):
+        self.name = name
+        self.defense = defense
+
+    def defend(self):
+        return random.randint(0, self.defense)
+
 
 class Team:
     name = ''
@@ -66,6 +117,51 @@ class Team:
     def view_all_heroes(self):
         for hero in self.heroes:
             print(hero.name)
+
+    def attack(self, other_team):
+        attack_power = 0
+        for hero in self.heroes:
+            attack_power += hero.attack()
+        kills = other_team.defend(attack_power)
+        self.update_kills(kills)
+
+    def defend(self, damage_amt):
+        defense_power = 0
+        for hero in self.heroes:
+            defense_power += hero.defend()
+        damage = damage_amt - defense_power
+        self.deal_damage(damage)
+        kills = 0
+        if damage > 0:
+            damage_per_hero = damage // len(self.heroes)
+            for hero in self.heroes:
+                dead = hero.take_damage(damage_per_hero)
+                if dead == 1:
+                    kills += 1
+        return kills
+
+    def deal_damage(self, damage):
+        deaths = 0
+        if damage > 0:
+            damage_per_hero = damage // len(self.heroes)
+            for hero in self.heroes:
+                dead = hero.take_damage(damage_per_hero)
+                if dead == 1:
+                    hero.deaths += 1
+                    deaths += 1
+        return deaths
+
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.health = hero.start_health
+
+    def stats(self):
+        for hero in self.heroes:
+            print("{}: \n       kills: {}\n       deaths: {}".format(hero.name, hero.kills, hero.deaths))
+
+    def update_kills(self, kills):
+        for hero in self.heroes:
+            hero.add_kill(kills)
 
 
 if __name__ == "__main__":
